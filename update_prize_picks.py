626b95767403fc23a931243b62453b4892e32192
Jack Driscoll <jdriscoll98@ufl.edu>
2025-02-23 15:29:09 -0500

    Add archiving functionality for daily Prize Picks props

--- update_prize_picks.py
+++ update_prize_picks.py
@@ -1,6 +1,15 @@
-
+from datetime import datetime, timedelta
 from prize_picks_scraper import PrizePicksAPI
 import json
+
+# read the current prize_picks_current_goblin_props.json file
+with open('prize_picks_current_goblin_props.json', 'r') as f:
+    current_props = json.load(f)
+
+# dump the current props to an archive file
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+with open(f'prize_picks_current_goblin_props_{yesterday}.json', 'w') as f:
+    json.dump(current_props, f) 
 
 prize_picks = PrizePicksAPI()
 props = prize_picks.get_current_goblin_props()
